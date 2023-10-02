6fdf8470e2ec11a2da45ae7fc11a4b47bfbe4cb7
HUMBERTO JOSE DANTAS <humbertojdantas@yahoo.com.br>
2023-10-02 12:17:05 -0300

    Criação da massa de teste DDT

--- selenium/SeleniumSimples.cs
+++ selenium/SeleniumSimples.cs
@@ -16,6 +16,27 @@ private IWebDriver driver; // objeto do Selenium WebDriver
 
 // 3.2 - Função ou Método de Apoio
 
+// Função de leitura do arquivo csv - massa de teste
+public static IEnumerable<TestCaseData> lerDadosDeTeste(){
+
+    // declaramos um objeto chamdo reader que lê o conteúdo do csv
+    using(var reader = new StreamReader(@"D:\ESTUDO\ITERASYS\FTS139\loja139\data\login.csv")){
+        // Pular a linha do cabeçalho do csv
+        reader.ReadLine();
+
+        // Faça enquanto não for o final do arquivo
+        // -- while -- ! -- reader.EndOfStream
+        while (!reader.EndOfStream)
+        {
+            // ler a linha correspondente - cortar a fileira do chocolate
+            var linha = reader.ReadLine();
+            var valores = linha.Split(",");
+
+            yield return new TestCaseData(valores[0], valores[1], valores[2]);
+        } // Fim do while - funciona como uma mola - (enquanto)
+    };
+
+}   
 
 // 3.3 - Configurações de Antes do Teste
 [SetUp] // Configura um método para ser executado antes dos testes
@@ -62,7 +83,29 @@ public void Login(){
 
 }// Fim do Login
 
+// Login Positivo DDT
+[Test, TestCaseSource("lerDadosDeTeste")] // Indica que é um método de teste
+public void LoginPositivoDDT(String usuario, String senha, String resultadoEsperado){
+    // Abrir o navegador e acessar o site
+    driver.Navigate().GoToUrl("https://www.saucedemo.com");
+    //Thread.Sleep(2000); // Pausa forçada - remover antes de publicar
 
+    // preencher o usuario
+    driver.FindElement(By.Id("user-name")).SendKeys(usuario);
+    // preencher a senha 
+    driver.FindElement(By.Name("password")).SendKeys(senha);
+    // clicar no botão Login
+    driver.FindElement(By.CssSelector("input.submit-button.btn_action")).Click();
+
+    // Verificar se fizemos o login no sistema, confirmando um texto ancora
+    Assert.AreEqual(driver.FindElement(By.CssSelector("span.title")).Text, resultadoEsperado);
+
+
+    Thread.Sleep(2000);
+
+
+
+}// Fim do Login Positivo DDT
 
 
 } // Fim da classe
